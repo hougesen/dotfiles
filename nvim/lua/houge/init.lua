8055a9abc5d3e276c3d40e377b36a73c2f60e973
Mads Hougesen <mads@mhouge.dk>
2023-10-28 18:25:27 +0200

    refactor(nvim): switch to lazy.nvim

--- nvim/lua/houge/init.lua
+++ nvim/lua/houge/init.lua
@@ -1,4 +1,56 @@
 require("houge.remap")
 require("houge.set")
 
-vim.cmd("colorscheme onedark")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	"wakatime/vim-wakatime",
+
+	{ "olimorris/onedarkpro.nvim", priority = 1000 },
+
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+	"hougesen/blame-me.nvim",
+
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	},
+
+	"echasnovski/mini.pairs",
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.4", dependencies = { "nvim-lua/plenary.nvim" } },
+}
+
+local opts = {
+	-- defaults = { lazy = true },
+	-- performance = {
+	-- cache = { enabled = true },
+	-- },
+}
+
+require("lazy").setup(plugins, opts)
+
+require("houge.colorscheme")
