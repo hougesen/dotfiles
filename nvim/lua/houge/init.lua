22850dee2e42437df1c8b354c983fdfd3d33fcf9
Mads Hougesen <mads@mhouge.dk>
2024-06-06 14:12:15 +0200

    feat(nvim): install zellij plugin

--- nvim/lua/houge/init.lua
+++ nvim/lua/houge/init.lua
@@ -67,6 +67,19 @@ local plugins = {
 	},
 
 	"b0o/schemastore.nvim",
+
+	{
+		"https://git.sr.ht/~swaits/zellij-nav.nvim",
+		lazy = true,
+		event = "VeryLazy",
+		keys = {
+			{ "<A-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+			{ "<A-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+			{ "<A-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+			{ "<A-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+		},
+		opts = {},
+	},
 }
 
 local opts = {
